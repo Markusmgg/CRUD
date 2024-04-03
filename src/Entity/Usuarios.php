28cd05536c2b27dbd45845972da23461e818ebce
Marcos <marcosgigorro@gmail.com>
2024-04-03 16:52:17 +0200

    first commit

---
+++ src/Entity/Usuarios.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UsuariosRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UsuariosRepository::class)]
+class Usuarios
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 9)]
+    private ?string $DNI = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nombre = null;
+
+    #[ORM\Column]
+    private ?int $telefono = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDNI(): ?string
+    {
+        return $this->DNI;
+    }
+
+    public function setDNI(string $DNI): static
+    {
+        $this->DNI = $DNI;
+
+        return $this;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): static
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getTelefono(): ?int
+    {
+        return $this->telefono;
+    }
+
+    public function setTelefono(int $telefono): static
+    {
+        $this->telefono = $telefono;
+
+        return $this;
+    }
+}
